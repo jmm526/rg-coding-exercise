1376743d243bbe818313c66062b17b9f79ae93f3
Jake Myers <jakemyers2016@u.northwestern.edu>
2018-08-21 13:05:36 -0500

    added search and filter functionality

--- src/components/container/PlayerTableContainer/PlayerTableContainer.js
+++ src/components/container/PlayerTableContainer/PlayerTableContainer.js
@@ -11,12 +11,21 @@ class PlayerTableContainer extends Component {
     players: [],
     sortColumn: null,
     sortUp: true,
+    checks: {
+      QB: true,
+      RB: true,
+      WR: true,
+      TE: true,
+    },
+    text: ''
   }
 
   componentDidMount = async () => {
     const players = await fetchPlayers();
     const ADP = await fetchADP();
-    const newNamePlayers = players.map(player => {
+
+    // Rearrange player names and add ADP's
+    this.players = players.map(player => {
       const nameArr = player.name.split(', ')
       player.name = nameArr[1] + ' ' + nameArr[0]
 
@@ -33,7 +42,69 @@ class PlayerTableContainer extends Component {
       return player
     })
 
-    this.setState({ ...this.state, players: newNamePlayers.sort(this.compareADP), sortColumn: 'ADP', sortUp: true });
+    // Create Trie
+    this.createTrie()
+
+    // Set state (default sorted by ADP)
+    this.setState({ ...this.state, players: this.players.sort(this.compareADP), sortColumn: 'ADP', sortUp: true });
+  }
+
+  updatePlayers = async () => {
+    const statePlayers = this.triePlayers.filter(player => {
+      return this.state.checks[player.position]
+    })
+    this.setState({ players: statePlayers });
+  }
+
+  // Create Trie for searching
+  createTrie = () => {
+    this.trie = {}
+    this.players.forEach(player => {
+      if (!this.trie[player.name.charAt(0)]) { this.trie[player.name.charAt(0)] = {} }
+      this.trieRec(this.trie[player.name.charAt(0)], player.name.slice(1), player)
+    })
+    this.triePlayers = this.players
+  }
+
+  // Recursive helper function for createTrie()
+  trieRec = (node, name, player) => {
+    if (name === '') { node.END = player }
+    else {
+      if (!node[name.charAt(0)]) { node[name.charAt(0)] = {} }
+      this.trieRec(node[name.charAt(0)], name.slice(1), player)
+    }
+  }
+
+  getPlayersTrie = () => {
+    if (!this.state.text) { return this.players }
+    let currNode = this.trie
+    for (let i = 0; i < this.state.text.length; i++) {
+      if (!currNode[this.state.text.charAt(i)]) { return [] }
+      currNode = currNode[this.state.text.charAt(i)]
+    }
+    const ret = []
+    this.getPlayerRec(currNode, ret)
+    return ret
+  }
+
+  getPlayerRec = (node, ret) => {
+    Object.keys(node).forEach(key => {
+      if (key === 'END') { ret.push(node[key] )}
+      else {
+        this.getPlayerRec(node[key], ret)
+      }
+    })
+  }
+
+  onChangeCheck = async (event) => {
+    await this.setState({...this.state, checks: {...this.state.checks, [event.target.name]: !this.state.checks[event.target.name]}})
+    this.updatePlayers()
+  }
+
+  onChangeText = async (event) => {
+    await this.setState({...this.state, text: event.target.value})
+    this.triePlayers = this.getPlayersTrie()
+    await this.updatePlayers()
   }
 
   compareADP(a, b) {
@@ -52,11 +123,11 @@ class PlayerTableContainer extends Component {
   }
 
   render() {
-    const { players } = this.state;
+    const { players, checks, text } = this.state;
     return (
       players.length ?
       <div>
-        <PlayerTableFilters />
+        <PlayerTableFilters checks={checks} text={text} onChangeCheck={this.onChangeCheck} onChangeText={this.onChangeText} />
         <PlayerTable players={players} onSort={this.onSort} sortColumn={this.state.sortColumn} sortUp={this.state.sortUp} />
       </div> :
       <Loading />
--- src/components/presentational/PlayerTableFilters/PlayerTableFilters.js
+++ src/components/presentational/PlayerTableFilters/PlayerTableFilters.js
@@ -3,16 +3,17 @@ import { FormGroup, Checkbox, FormControl } from 'react-bootstrap';
 
 import './PlayerTableFilters.css';
 
-const PlayerTableFilters = () => {
+const PlayerTableFilters = (props) => {
+  const {checks, onChangeCheck, onChangeText} = props
   return (
     <div className="PlayerTableFilters">
       <FormGroup>
-        <Checkbox inline>QB</Checkbox>
-        <Checkbox inline>RB</Checkbox>
-        <Checkbox inline>WR</Checkbox>
-        <Checkbox inline>TE</Checkbox>
+        <Checkbox name='QB' onChange={onChangeCheck} checked={checks['QB']} inline>QB</Checkbox>
+        <Checkbox name='RB' onChange={onChangeCheck} checked={checks['RB']} inline>RB</Checkbox>
+        <Checkbox name='WR' onChange={onChangeCheck} checked={checks['WR']} inline>WR</Checkbox>
+        <Checkbox name='TE' onChange={onChangeCheck} checked={checks['TE']} inline>TE</Checkbox>
       </FormGroup>
-      <FormControl type="text" placeholder="Search by Name" />
+      <FormControl type="text" placeholder="Search by Name" onChange={onChangeText} />
     </div>
   )
 }
